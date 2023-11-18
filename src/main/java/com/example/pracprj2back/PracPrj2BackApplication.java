617e2a8ee56a84161487684cd3cd2e06b4435b55
LeeJungHoon <gns14585@naver.com>
2023-11-18 12:17:21 +0900

    최초 설정 및 BoardController post /api/board/add 작성

---
+++ src/main/java/com/example/pracprj2back/PracPrj2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pracprj2back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracPrj2BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracPrj2BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pracprj2back/PracPrj2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pracprj2back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracPrj2BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
