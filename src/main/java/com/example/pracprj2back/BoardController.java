757370303fbe097edf9b343b0931dff326d50421
LeeJungHoon <gns14585@naver.com>
2023-11-18 12:30:39 +0900

    새 게시글 쓰기 controller, service, mapper, dto, table

--- src/main/java/com/example/pracprj2back/BoardController.java
+++ src/main/java/com/example/pracprj2back/BoardController.java
@@ -1,7 +1,10 @@
 package com.example.pracprj2back;
 
+import com.example.pracprj2back.domain.Board;
+import com.example.pracprj2back.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/board")
 public class BoardController {
 
+    private final BoardService service;
+
     @PostMapping("add")
-    public void add() {
-        System.out.println("BoardController.add");
+    public void add(@RequestBody Board board) {
+        service.save(board);
     }
 }
 
