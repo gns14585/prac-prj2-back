08a65e176a35abfac9110e45220cd8292cdca4f4
LeeJungHoon <gns14585@naver.com>
2023-11-18 17:53:25 +0900

    회원가입 기능 완성

---
+++ src/main/java/com/example/pracprj2back/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.example.pracprj2back.mapper;
+
+import com.example.pracprj2back.domain.Member;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberMapper {
+
+    @Insert("""
+            INSERT INTO member (id, password, email)
+            VALUES (#{id}, #{password}, #{email})
+            """)
+    int insert(Member member);
+}
---
+++ src/main/java/com/example/pracprj2back/service/MemberService.java
@@ -0,0 +1,18 @@
+package com.example.pracprj2back.service;
+
+import com.example.pracprj2back.controller.MemberController;
+import com.example.pracprj2back.domain.Member;
+import com.example.pracprj2back.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberMapper mapper;
+
+    public void add(Member member) {
+        mapper.insert(member);
+    }
+}
