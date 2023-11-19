483850cd92ea571d7be6747ce940983f9b59df32
LeeJungHoon <gns14585@naver.com>
2023-11-19 18:46:39 +0900

    회원정보 수정 기능

--- src/main/java/com/example/pracprj2back/controller/MemberController.java
+++ src/main/java/com/example/pracprj2back/controller/MemberController.java
@@ -72,4 +72,30 @@ public class MemberController {
             return ResponseEntity.internalServerError().build();
         }
     }
+
+    @PutMapping("edit")
+    public ResponseEntity edit(@RequestBody Member member) {
+        // TODO : 로그인 했는지? 자기정보인지?
+
+        if (service.update(member)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.internalServerError().build();
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/example/pracprj2back/mapper/MemberMapper.java
+++ src/main/java/com/example/pracprj2back/mapper/MemberMapper.java
@@ -1,10 +1,7 @@
 package com.example.pracprj2back.mapper;
 
 import com.example.pracprj2back.domain.Member;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -50,4 +47,20 @@ public interface MemberMapper {
             WHERE id = #{id}
             """)
     int deleteById(String id);
+
+    // 수정버튼을 눌렀을때 password가 그대로인 상태라면 빈칸으로 두고 수정버튼을 누르는데
+    // if문이 없으면 패스워드는 빈칸으로 바뀌게됨
+    // 그래서 if문 안에 있는 코드는 password가 변경되고싶지 않을때에 대비한 코드
+    @Update("""
+            <script>
+            UPDATE member
+            SET 
+                <if test="password != ''">
+                password = #{password},
+                </if>
+                email = #{email}
+            WHERE id = #{id}
+            </script>
+            """)
+    int update(Member member);
 }
